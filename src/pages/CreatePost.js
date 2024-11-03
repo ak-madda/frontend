627107c161fe9a5106029ba0659298d62e8d0d3b
ak-madda <abdemadda@gmail.com>
2024-11-03 19:41:19 +0100

    second commit with pages

---
+++ src/pages/CreatePost.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PostForm from "../components/PostForm";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <h1>Create or Edit Post</h1>
+      <PostForm />
+    </div>
+  );
+};
+
+export default CreatePost;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Login from "../components/Login";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login or Register</h1>
+      <Login />
+    </div>
+  );
+};
+
+export default Login;
